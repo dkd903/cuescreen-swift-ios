dd59a251a04eb1a07181fc2e6762bb40b63f13b4
Debjit Saha <debjitsaha87@gmail.com>
2014-11-19 20:19:41 -0500

    Initial Commit

---
+++ CueScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CueScreen
+//
+//  Created by Debjit Saha on 11/19/14.
+//  Copyright (c) 2014 com.cuescreen.app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
